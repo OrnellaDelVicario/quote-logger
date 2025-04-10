7d03ab8dd0d3be47741c5f9c6c5d4ca9c41ffbe7
OrnellaDelVicario <ornelladelvicario@gmail.com>
2025-04-10 12:51:43 +0200

    Update Quotes.java

--- Quotes.java
+++ Quotes.java
@@ -1,5 +1,5 @@
 public class Quotes {
     public static void main(String[] args) {
-        System.out.println("Your Name: \"Your first awesome quote here!\"");
+        System.out.println("Zora Neale Hurston: \"There are years that ask questions and years that answer.\"");
     }
 }
